a28464fb19626ec5417ed8d4fc6c5fa38c900ec3
Devine Lu Linvega <aliceffekt@gmail.com>
2018-03-10 10:16:11 +1300

    Added list

--- scripts/templates/index.js
+++ scripts/templates/index.js
@@ -8,14 +8,42 @@ function IndexTemplate(id,rect)
 
   this.answer = function(q)
   {
-    console.log("!!")
     return {
       title:`GrimGrains — Index`,
       view:{
         core: {
-          content: `Hello`
+          content: make_list(q.tables.recipes)
         }
       }
     }
+  }
+
+  function make_list(recipes)
+  {
+    var html = ""
+
+    // Sort by tag
+
+    var categorized = {}
+
+    for(name in recipes){
+      var recipe = recipes[name]
+      if(!categorized[recipe.TAGS[0]]){ categorized[recipe.TAGS[0]] = []; }
+      recipe.name = name
+      categorized[recipe.TAGS[0]].push(recipe)
+    }
+
+    for(cat in categorized){
+      var recipes = categorized[cat]
+      html += `<h3>${cat.capitalize()}</h3>`
+      html += "<list style='margin-bottom:15px'>"
+      for(id in recipes){
+        var recipe = recipes[id]
+        html += `<ln>${recipe.name.capitalize()}</ln>`
+
+      }
+      html += "</list>"
+    }
+    return `<columns id='index'>${html}</columns>`;
   }
 }
