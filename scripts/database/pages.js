ee313d9e124267ee7fabd866e56d4a57331f869e
Rekka <rekkabell@gmail.com>
2018-07-23 19:18:27 +1200

    shears

--- scripts/database/pages.js
+++ scripts/database/pages.js
@@ -41,8 +41,11 @@ TOOLS
     & If you have no intention of grinding nuts and herbs, a marble mortar with a smooth surface will do fine. Wood mortars are discouraged, that is, unless you want to re-use it for the same purpose over and over again. Flavor lingers into the bowl, and may result in unwanted tastes in other recipes. There are other materials to consider, but all in all, a heavy, textured, granite bowl is your best bet as a good all-around mortar. 
     * Cleaver
     & The cleaver, or the chinese chef's knife, is a less brutish version of the meat cleaver. This tool has a wide variety of purposes, with it, you can dice, slice and julienne vegetables. After the item has been cut into bits, you can use the wide blade to scoop everything up. There is no real need for other knives in your artillery. 
-    & When selecting your cleaver, aim for a harder grade of steel – somewhere between 57-58 on the {{Rockwell Hardness Scale|https://en.wikipedia.org/wiki/Rockwell_scale}}. A good steel produces a finer edge, and holds its sharpness. The angle of the blade is also important, 22 degrees is most common.
+    & When selecting your cleaver, aim for a harder grade of steel – somewhere between 57-58 on the {{Rockwell Hardness Scale|https://en.wikipedia.org/wiki/Rockwell_scale}}. Good steel produces a finer edge, and holds its sharpness. The angle of the blade is also important, 22 degrees is most common and this too, ensures longer-lasting sharpness.
     & The handle of the cleaver should not be too thick or too small, the size has to allow the hand to wrap around it and to just touch the other side of your thumb. Traditionally, Chinese cleavers are made of carbon steel, but because these are prone to rust stainless steel is preferred (a mixture of carbon and SS is also good).
+    * Shears (Nigiri)
+    & Traditional Japanese shears, or Nigiri, are forged using a single piece of metal and spring back to the open position when released. While the design is simple and durable, it offers more precision than scissors. Nigiri can be used to cut some herbs from the garden to add to a meal, or cutting dried mushrooms or chilies into smaller bits, there are over 100 uses for it. 
+    & Most shears are made from carbon steel, stainless steel is again, preferable. A pair with blades that make more than half of the length of the tool, will permit for a greater variety of uses. The shears rest inside your hand, between your index finger and thumb, giving greater control of the speed of the cut. This tool, is also ideal for left-handed people, making cutting a pain-free experience. 
 
 INGREDIENTS
   BREF : Here is an unsorted list of all the ingredients used in GrimGrains recipes!
@@ -56,10 +59,8 @@ DISCLAIMER
     & You may adapt one of my grim creations, but be sure to link back to the original recipe and to give proper credit.
     & Opinions expressed by any third parties are theirs and do not reflect my own.
     * Disclaimer
-    & All recipes featured on grimgrains.com are my own, unless stated otherwise. The information is for food enthusiasts like myself, i do not claim to be an all-knowing food guru. Nor am I a health professional. My views are my own, i encourage curiosity whenever possible.
+    & All recipes featured on grimgrains.com are my own, unless stated otherwise. The information is for food enthusiasts like myself, i do not claim to be all-knowing. Nor am I a health professional. My views are my own, i encourage curiosity whenever possible.
     * Privacy statement
-    & Ads on the website are from third parties such as google, who may store information about your online coming and goings to provide personalized advertisements. All concerns about browsing privacy should be addressed to google.
-    & Any visitor personal information will be kept private and will not be shared/sold to another party. I am not responsible for the privacy practices or actions of any of my blog commenters.
-    & I reserve the right to alter the blog at my own discretion, as well as to filter comments without notice.
+    & I reserve the right to alter the blog at my own discretion.
     & Words addressed to me in private will not be shared, nor will i use any of it in future publications.
 `
