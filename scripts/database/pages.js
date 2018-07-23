e24d0b0a4a5f9794f248440485c7b204fb9bd4a1
Rekka <rekkabell@gmail.com>
2018-07-23 15:09:34 +1200

    tools page

--- scripts/database/pages.js
+++ scripts/database/pages.js
@@ -19,6 +19,28 @@ ABOUT
     & I reserve the right to alter the blog at my own discretion, as well as to filter comments without notice.
     & Words addressed to me in private will not be shared, nor will i use any of it in future publications.
 
+TOOLS
+  BREF : Entering a store, with the goal of purchasing kitchenware can be a dizzying experience. I made this list of notes on what to look for in a tool, based on experience and research: 
+  LONG
+    & Durability- choose a material that will last, or that can be composted.
+    & Simplicity - prioritize unibody tools, they don't break as easily. The fewer parts the better, opting for non-electric is important for that same reason.
+    & Multi-functional - having too many tools that have a single function, adds clutter to our home and in our minds. Its best to do away with it.
+    * Cutting boards
+    & Opt for a maple or bamboo hardwood cutting board. Avoid plastic. Contrary to popular belief, it is not more sanitary and can harbour a lot of bacteria. A knife-scarred plastic surface is difficult to disinfect, and studies have shown that washing the board – whether by hand or with a dishwasher – was not enough to kill lingering bacteria. 
+    & Wood doesn't scar as easily, and is the preferred material. After cleaning, it must be allowed to dry thoroughly. 
+    & _________________________________
+    & {{Comparative studies on hygienic qualities of wood and plastic cutting boards in a laboratory|https://www.johnboos.com/mailchimppdf/WOODvsPLASTIC-GERMANSTUDY2015-V1.pdf}} Dr. Ulrike Kleiner, the Laboratory of Hygiene Research at the Anhalt University in Bernburg, Germany
+    & {{Plastic and wooden cutting boards|media/blog/ucdavisfoodsafety.cuttingboard.pdf}} Dr. Dean O. Cliver, University of California, USA
+    * Manual coffee grinder
+    & If you enjoy coffee, getting a grinder should be on the top of your list. Grinding your own beans, means you can choose the size of the grind to match the selected brewing method. 
+    & Choose a manual grinder with ceramic burrs, the two plates crush the beans into even grinds, and is the best way to release the oils that are responsible for the flavour of the coffee. Steel burrs, while cheaper, don't last as long as the ceramic kind. Aim for long-lasting, rather than cheap. As for blade grinders, they grind unevenly and will not do. 
+    & _________________________________
+    & {{The effect of bean origin and temperature on grinding roasted coffee|https://www.nature.com/articles/srep24483}} Uman, E. et al. Sci. Rep. 6, 24483; doi: 10.1038/srep24483, 2016
+    * Mortar and pestle
+    & Unlike other types of tools, your intended use will determine the choice of material of your future mortar and pestle. Will you grind spices with it? Nuts? Fibrous herbs? The best material, for the most varied jobs, is granite. Its weight and irregular surface makes it easy to grind most spices, nuts, and can handle fibrous herbs well enough. 
+    & If you have no intention of grinding nuts and herbs, a marble mortar with a smooth surface will do fine. Wood mortars are discouraged, that is, unless you want to re-use it for the same purpose over and over again. Flavor lingers into the bowl, and may result in unwanted tastes in other recipes. There are other materials to consider, but all in all, a heavy, textured, granite bowl is your best bet as a good all-around mortar. 
+
+
 INGREDIENTS
   BREF : Here is an unsorted list of all the ingredients used in GrimGrains recipes!
   LONG
