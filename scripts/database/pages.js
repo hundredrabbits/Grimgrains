139c2a72e343b7b19ed9cec5ab79cf41aefdb392
Rekka <rekkabell@gmail.com>
2018-07-24 14:49:58 +1200

    cookware

--- scripts/database/pages.js
+++ scripts/database/pages.js
@@ -24,33 +24,46 @@ TOOLS
     & {*Multi-functional*}: Having too many tools that have a single function, adds clutter to our home and in our minds.  
     % pages/tools.jpg
     * Cutting boards
-    & Opt for a maple or bamboo hardwood cutting board. Avoid plastic. Contrary to popular belief, it is not more sanitary and can harbour a lot of bacteria. A knife-scarred plastic surface is difficult to disinfect, and studies have shown that washing the board – whether by hand or with a dishwasher – was not enough to kill lingering bacteria. 
+    & {*Material*}: Opt for a maple or bamboo hardwood cutting board. Avoid plastic. Contrary to popular belief, it is not more sanitary and can harbour a lot of bacteria. A knife-scarred plastic surface is difficult to disinfect, and studies have shown that washing the board – whether by hand or with a dishwasher – was not enough to kill lingering bacteria. 
     & Wood doesn't scar as easily, and is the preferred material. After cleaning, it must be allowed to dry thoroughly. 
     > <hr class='stroke' />
     & {{Comparative studies on hygienic qualities of wood and plastic cutting boards in a laboratory|https://www.johnboos.com/mailchimppdf/WOODvsPLASTIC-GERMANSTUDY2015-V1.pdf}} Dr. Ulrike Kleiner, the Laboratory of Hygiene Research at the Anhalt University in Bernburg, Germany
     & {{Plastic and wooden cutting boards|media/blog/ucdavisfoodsafety.cuttingboard.pdf}} Dr. Dean O. Cliver, University of California, USA
     * Manual coffee grinder
     & If you enjoy {{coffee|#coffee}}, getting a grinder should be on the top of your list. Grinding your own beans, means you can choose the size of the grind to match the selected brewing method. 
-    & Choose a manual grinder with ceramic burrs, the two plates crush the beans into even grinds, and is the best way to release the oils that are responsible for the flavour of the coffee. Steel burrs, while cheaper, don't last as long as the ceramic kind. Aim for long-lasting, rather than cheap. As for blade grinders, they grind unevenly and will not do. 
+    & {*Material*}: Choose a manual grinder with ceramic burrs, the two plates crush the beans into even grinds, and is the best way to release the oils that are responsible for the flavour of the coffee. Steel burrs, while cheaper, don't last as long as the ceramic kind. Aim for long-lasting, rather than cheap. As for blade grinders, they grind unevenly and will not do. 
     > <hr class='stroke' />
     & {{The effect of bean origin and temperature on grinding roasted coffee|https://www.nature.com/articles/srep24483}} Uman, E. et al. Sci. Rep. 6, 24483; doi: 10.1038/srep24483, 2016
     * Mortar and pestle
-    & Unlike other types of tools, your intended use will determine the choice of material of your future mortar and pestle. Will you grind spices with it? Nuts? Fibrous herbs? The best material, for the most varied jobs, is granite. Its weight and irregular surface makes it easy to grind most spices, nuts, and can handle fibrous herbs well enough. 
+    & Unlike other types of tools, your intended use will determine the choice of material of your future mortar and pestle. Will you grind spices with it? Nuts? Fibrous herbs? 
+    & {*Material*}: The best material, for the most varied jobs, is granite. Its weight and irregular surface makes it easy to grind most spices, nuts, and can handle fibrous herbs well enough. 
     & If you have no intention of grinding nuts and herbs, a marble mortar with a smooth surface will do fine. Wood mortars are discouraged, that is, unless you want to re-use it for the same purpose over and over again. Flavor lingers into the bowl, and may result in unwanted tastes in other recipes. There are other materials to consider, but all in all, a heavy, textured, granite bowl is your best bet as a good all-around mortar. 
     * Cleaver
     & The cleaver, or the chinese chef's knife, is a less brutish version of the meat cleaver. This tool has a wide variety of purposes, with it, you can dice, slice and julienne vegetables. After the item has been cut into bits, you can use the wide blade to scoop everything up. There is no real need for other knives in your artillery. 
-    & When selecting your cleaver, aim for a harder grade of steel – somewhere between 57-58 on the {{Rockwell Hardness Scale|https://en.wikipedia.org/wiki/Rockwell_scale}}. Good steel produces a finer edge, and holds its sharpness. The angle of the blade is also important, 22 degrees is most common and this too, ensures longer-lasting sharpness.
-    & The handle of the cleaver should not be too thick or too small, the size has to allow the hand to wrap around it and to just touch the other side of your thumb. Traditionally, Chinese cleavers are made of carbon steel, but because these are prone to rust stainless steel is preferred (a mixture of carbon and SS is also good).
+    & {*Material*}: When selecting your cleaver, aim for a harder grade of steel – somewhere between 57-58 on the {{Rockwell Hardness Scale|https://en.wikipedia.org/wiki/Rockwell_scale}}. Good steel produces a finer edge, and holds its sharpness. The angle of the blade is also important, 22 degrees is most common and this too, ensures longer-lasting sharpness. Traditionally, Chinese cleavers are made of carbon steel, but because these are prone to rust stainless steel is preferred (a mixture of carbon and SS is also good).
+    & {*Shape*}: The handle of the cleaver should not be too thick or too small, the size has to allow the hand to wrap around it and to just touch the other side of your thumb. 
     * Shears (Nigiri)
-    & Traditional Japanese shears, or Nigiri, are forged using a single piece of metal and spring back to the open position when released. While the design is simple and durable, it offers more precision than scissors. Nigiri can be used to cut some herbs from the garden to add to a meal, or cutting dried mushrooms or chilies into smaller bits, there are over 100 uses for it. 
-    & Most shears are made from carbon steel, stainless steel is again, preferable. A pair with blades that make more than half of the length of the tool, will permit for a greater variety of uses. The shears rest inside your hand, between your index finger and thumb, giving greater control of the speed of the cut. This tool, is also ideal for left-handed people, making cutting a pain-free experience. 
+    & Traditional Japanese shears, or Nigiri, can be used to cut some herbs from the garden to add to a meal, or cutting dried mushrooms or chilies into smaller bits, there are over 100 uses for it.  
+    & {*Material*}: Most shears are made from carbon steel, stainless steel is again, preferable.
+    & {*Shape*}: Nigiri are forged using a single piece of metal and spring back to the open position when released. While the design is simple and durable, it offers more precision than scissors. A pair with blades that make more than half of the length of the tool, will permit for a greater variety of uses. The shears rest inside your hand, between your index finger and thumb, giving greater control of the speed of the cut. This tool, is also ideal for left-handed people, making cutting a pain-free experience. 
     * Rolling pin
-    & A simple tool, essential for baking at home. A good rolling pin will not have handles, and will often cost less. According to some experts, having no handles gives a better sense of the dough. Aim for a tapered french-style pin, its shape permits rolling the dough into rounds and oblongs with ease. Choosing a hardwood like maple is recommended, because of its strength and because of how easy it is to clean. 
+    & A simple tool, essential for baking at home. 
+    & {*Material*}: Choosing a hardwood like maple is recommended, because of its strength and because of how easy it is to clean. 
+    & {*Shape*}: A good rolling pin will not have handles, and will often cost less. According to some experts, having no handles gives a better sense of the dough. Aim for a tapered french-style pin, its shape permits rolling the dough into rounds and oblongs with ease. 
     > <hr class='stroke' />
     & {{The best rolling pin|https://thewirecutter.com/reviews/best-rolling-pin/}} Wirecutter, 2017
     * Pressure cooker
-    * Cooking pan
-    & MORE TOOLS TO BE ADDED SOON...
+    & A stovetop pressure cooker will help save time and energy, while providing you with good meals quickly. Instead of a meal simmering for a hours, all can be done within an hour. 
+    & {*Material*}: Aim for Stainless Steel.
+    & {*Shape*}: Pick an 8-Quartmodel, with a spring-loaded valve and pressure indicator. If you chose well, this is a tool that you will follow you into old age. A model with a thicker base, of at least 7mm, will retain heat better, reach pressure more quickly and will keep food from burning. Pick a cooker that offers a low pressure setting of 8-9 PSI per square inch  and a high pressure setting of 15PSI, anything else is below standard, and will result in your food not cooking well. A pressure cooker can also serve as a regular large stock-pot without the lid.
+    > <hr class='stroke' />
+    & {{The best pressure cooker|https://thewirecutter.com/reviews/best-pressure-cooker/}} Wirecutter, 2018
+    & {{Stovetop pressure cooker|http://www.cooksillustrated.com/equipment_reviews/1398-stovetop-pressure-cookers?ref=new_search_experience_1&incode=MCSCD00L0}} Cook’s Illustrated, 2013
+    & {{Test Drive: Pressure Cookers|http://www.finecooking.com/articles/test-drive-pressure-cookers.aspx?pg=0}} Nicki Pendleton Wood, Fine Cooking, 2011
+    * Skillet
+    & Skillets are used to stir-fry, bake, simmer and cook a grand variety of dishes.
+    & {*Material*}: Cast-iron is the material of choice, it is durable, cheaper, and offers more versatility than stainless steel skillets. It can be used to cook in the oven, safe past 500 degrees. Cast-iron cookware gets better with time as it develops layers of seasoning. Buying a pre-seasoned skillet is best.
+    & {*Shape*}: A large cooking area is ideal, with large spouts to pour out contents with ease. The skillet will be heavy, rounded-stick handles make it easier and more comfortable to lift.
 
 INGREDIENTS
   BREF : Here is an unsorted list of all the ingredients used in GrimGrains recipes!
