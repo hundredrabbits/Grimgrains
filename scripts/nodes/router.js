f22b6d798fd9ba4e60c39d6681cbfec0fd854330
Devine Lu Linvega <aliceffekt@gmail.com>
2019-01-08 15:23:48 +1200

    Standardized

--- scripts/nodes/router.js
+++ scripts/nodes/router.js
@@ -1,39 +1,36 @@
-function RouterNode(id,rect)
-{
-  Node.call(this,id,rect);
+function RouterNode (id, rect) {
+  Node.call(this, id, rect)
 
   this.glyph = NODE_GLYPHS.router
 
-  this.receive = function(q)
-  {
-    var target = q.indexOf(":") > -1 ? q.split(":")[0].replace(/\+/g," ") : q.replace(/\+/g," ")
-    var params = q.indexOf(":") > -1 ? q.split(":")[1] : null
-    var db = this.request("database").database;
-    var data = find(target.toUpperCase(),db)
+  this.receive = function (q) {
+    var target = q.indexOf(':') > -1 ? q.split(':')[0].replace(/\+/g, ' ') : q.replace(/\+/g, ' ')
+    var params = q.indexOf(':') > -1 ? q.split(':')[1] : null
+    var db = this.request('database').database
+    var data = find(target.toUpperCase(), db)
 
     this.label = `${this.id}|${target}|${params}`
 
-    console.log(this.id,`${data ? data.type : '?'}->${target}[${params}]`);
+    console.log(this.id, `${data ? data.type : '?'}->${target}[${params}]`)
 
     this.send({
-      name:target,
-      type:data ? data.type : null,
-      result:data ? data.result : null,
-      params:params,
-      tables:db
+      name: target,
+      type: data ? data.type : null,
+      result: data ? data.result : null,
+      params: params,
+      tables: db
     })
   }
 
-  function find(key,db)
-  {
-    if(parseInt(key) > 0){ return null; }
-    
-    for(id in db){
+  function find (key, db) {
+    if (parseInt(key) > 0) { return null }
+
+    for (id in db) {
       var table = db[id]
-      if(table[key]){
-        return {type:id,result:table[key]}
+      if (table[key]) {
+        return { type: id, result: table[key] }
       }
     }
-    return {type:null,result:null}
+    return { type: null, result: null }
   }
 }
--- scripts/nodes/template.js
+++ scripts/nodes/template.js
@@ -1,22 +1,20 @@
-function TemplateNode(id,rect)
-{
-  Node.call(this,id,rect);
+function TemplateNode (id, rect) {
+  Node.call(this, id, rect)
 
   this.glyph = NODE_GLYPHS.parser
 
-  this.cache = null;
+  this.cache = null
 
-  this.receive = function(q)
-  {
-    var assoc = this.signal(q.type ? q.type.slice(0, -1) : "page");  
+  this.receive = function (q) {
+    var assoc = this.signal(q.type ? q.type.slice(0, -1) : 'page')
     var payload = assoc.answer(q)
 
     this.send(payload)
     this.label = `template:${assoc.id}`
-  
-    // Install Dom
-    document.body.appendChild(this.signal("view").answer())
 
-    setTimeout(()=>{ Ø("view").el.className = `${q.name} ready` },250)
+    // Install Dom
+    document.body.appendChild(this.signal('view').answer())
+
+    setTimeout(() => { Ø('view').el.className = `${q.name} ready` }, 250)
   }
 }
