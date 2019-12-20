925c71f36e64a724e34adab72649e6565d06dc02
neauoire <aliceffekt@gmail.com>
2019-12-20 10:26:22 -0500

    Working on migrating the db

--- source/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cc -std=c99 -DDEBUG -Wall -Wpedantic -Wshadow -Wextra -Werror=implicit-int -Werror=incompatible-pointer-types -Werror=int-conversion -lm -Og -fsanitize=address -fsanitize=undefined main.c -o main
-
-./main
