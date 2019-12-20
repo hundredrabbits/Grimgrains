f2935a50c12af8f895c06cb4ed293917a9abb0dc
neauoire <aliceffekt@gmail.com>
2019-12-20 12:12:12 -0500

    Migration done.

--- src/build.sh
+++ src/build.sh
@@ -3,3 +3,5 @@
 cc -std=c99 -DDEBUG -Wall -Wpedantic -Wshadow -Wextra -Werror=implicit-int -Werror=incompatible-pointer-types -Werror=int-conversion -lm -Og -fsanitize=address -fsanitize=undefined main.c -o main
 
 ./main
+
+rm ./main
