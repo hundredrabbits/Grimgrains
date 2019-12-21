6acff08e66eb2f1f4c2af1d1ed5c8412dc1602b4
Rekka <rekkabell@gmail.com>
2019-12-21 15:10:36 -0500

    New recipe test

--- src/recipes.c
+++ src/recipes.c
@@ -1,3 +1,27 @@
+// veggie pate
+Recipe veggie_pate = create_recipe("veggie pate", "sidedish", "4 blocks", 20191220, 60);
+set_description(&veggie_pate, "Végé pâté is a quebec staple food, it's also one of those things that most people never think to make yourself. Every picnic I had this summer included this, it's great with veggies, on crackers and in sandwiches too! Purchasing it pre-made can be expensive, and not all brands are good. <br /><br /><b>Recommendations</b><br /><br />A lot of végé pâté recipes call for whole wheat flour, I've made pâté with it before with great results. If you don't have oats, you can use whole wheat flour instead. Using oats as a substitute works great, I always have a bucket-load of rolled oats lying around. To grind the rolled or quick oats oats down into flour, I use my mortar and pestle, but a handstick blender would work well too.<br /><br />");
+RecipePart veggie_pate_preparation = create_part("preparation");
+add_instruction(&veggie_pate_preparation, "Soak <i>1 cup</i> of raw unsalted <a href='sunflower_seeds'>sunflower seeds</a> in water for <u>2 hours</u>. Then, drain and rinse well and transfer to a big bowl. Using an immersion blender, puree the seeds. Set aside.");
+add_instruction(&veggie_pate_preparation, "Preheat oven to <u>350F</u>.");
+add_instruction(&veggie_pate_preparation, "Grate enough <a href='sweet_potatoes.html'>Sweet potatoes</a> to fill <i>2 cups</i>, then pour into the big bowl with the sunflower seeds.");
+add_instruction(&veggie_pate_preparation, "Chop <i>1</i> of <a href=yellow.onion.html'>yellow onion</a> and <i>3</i> <a href='garlic.html'>garlic cloves</a>. Bring a pan to medium heat, add <i>1 tbsp</i> of <a href='olive_oil.html'>olive oil</a> and the chopped yellow onion. Sautée until browned, then add minced garlic and cook for another minute. Pour contents of pan into the big bowl.");
+add_instruction(&veggie_pate_preparation, "Pulse <i>3/4 cup<i> of <a href='rolled_oats'>rolled oats</a> into a fine powder using an immersion blender or mortar and pestle, this should give you <i>1/2 cup</i> of oat powder. Add oat powder into the big bowl.");
+add_instruction(&veggie_pate_preparation, "Add <i>2 tbsp</i> of <a href='soy_sauce.html'>soy sauce</a>, <i>1 tsp</i> of <a href='lemon_juice'>lemon juice</a>, <i>1 tsp</i> of <a href='thyme.html'>dried thyme</a>, <i>1/3 cup</i> of <a href='water.html'>water</a> to the rest of the ingredients. Blend everything using an immersion blender. Then add <i>a pinch</i> of <a href='black pepper'>black pepper</a> <i>1/4 cup</i> of <a href='nutritional_yeast'>nutritional yeast</a>, and mix it in with a spoon. ");
+add_instruction(&veggie_pate_preparation, "Pour mixture into a grease 8x8 baking dish, and bake for <u>50 minutes</u>. Let cool before serving. Keeps for a week in the fridge, and even longer if frozen.");
+add_serving(&veggie_pate_preparation, &sunflower_seeds, "1 cup");
+add_serving(&veggie_pate_preparation, &sweet_potatoes, "2 cups, grated");
+add_serving(&veggie_pate_preparation, &olive_oil, "1 tsp");
+add_serving(&veggie_pate_preparation, &yellow_onion, "1");
+add_serving(&veggie_pate_preparation, &garlic, "3 cloves");
+add_serving(&veggie_pate_preparation, &rolled_oats, "3/4 cup, ground");
+add_serving(&veggie_pate_preparation, &soy_sauce, "2 tbsp");
+add_serving(&veggie_pate_preparation, &lemon_juice, "1 tsp");
+add_serving(&veggie_pate_preparation, &thyme, "1 tsp");
+add_serving(&veggie_pate_preparation, &black_pepper, "to taste");
+add_serving(&veggie_pate_preparation, &water, "1/3 cup");
+add_part(&veggie_pate, &veggie_pate_preparation);
+
 // tzaziki
 Recipe tzaziki = create_recipe("tzaziki", "sauce", "8 servings", 20191206, 10);
 set_description(&tzaziki, "Making tzaziki is easy. This is a basic recipe that works well enough with all kinds of substitutions, depending on dietary restrictions.<br /><br /><b>Substitutions</b><br /><br />It is possible to use <a href='sunflower_seeds.html'>sunflower seeds</a> and <a href='tofu.html'>tofu</a> to replace the hemp seeds. In this recipe, I used half hemp and half soy, because hemp, while being nutritious, is not cheap. Using just <a href='shelled_hemp_seeds.html'>shelled hemp seeds</a> is very possible.<br /><br />If using sunflower seeds, you should pre-soak them for 1-2h prior to blending. If using only tofu, you may not require extra water in the recipe.<br /><br />");
